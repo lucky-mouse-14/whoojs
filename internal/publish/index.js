3dbe14df6e649fdf78cfa5060ea704e8cbf66d0e
Tony Wu <1058011652@qq.com>
2023-05-19 18:35:52 +0800

    fix bug

--- internal/publish/index.js
+++ internal/publish/index.js
@@ -3,6 +3,6 @@ import { pkgPath } from "../utils/paths";
 import { series } from "gulp";
 
 export const publishComponent = async () => {
-  run("release-it", `${pkgPath}/easyest`);
+  run("release-it", `${pkgPath}/whoojs`);
 };
 export default series(async () => publishComponent());
